22ea401d9397fc145f5a25ed5247010f369f3716
League Student <league@iMac-9.attlocal.net>
2019-10-13 12:54:31 -0700

    Update SecretMessageBox.java

--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -1,16 +1,16 @@
 package _04_secret_message_box;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
 /**
  * Secret Message Box / Secure Messaging System
  * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
+ * You want to leave a message on one of the Mac computers so that only your
+ * friend can read it. You set up the passcode and the secret message. Your
+ * friend types in the passcode to retrieve the message.
  * 
  */
 
@@ -18,16 +18,23 @@ public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
 	public static void main(String[] args) {
-		String password="lily";
-	// 1. Set a password in a String variable
-	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-String j=JOptionPane.showInputDialog("can I have a secret message?");
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
-JOptionPane.showMessageDialog(null, j);
-	// 4. If their guess matches the password, show them the secret message
-if()
-	// 5. If the password does not match, pop-up "INTRUDER!!"
+		String password = "lily";
+		// 1. Set a password in a String variable
+		// 2. Using a pop-up, ask the first person for a secret message and store it in
+		// a variable
+		String j = JOptionPane.showInputDialog("can I have a secret message?");
+		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret
+		// message
+		// if they can guess the passcode
+		String passcode = JOptionPane.showInputDialog("guess password");
+		// 4. If their guess matches the password, show them the secret message
+		if (passcode.equals(password)) {
 
+			JOptionPane.showMessageDialog(null, "this is your message");
+		}
+		// 5. If the password does not match, pop-up "INTRUDER!!"
+		else {
+			JOptionPane.showMessageDialog(null, "intruder");
+		}
 	}
 }
