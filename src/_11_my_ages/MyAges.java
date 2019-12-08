c3559ff0852b4f0919693eec2cd9d34c818b796d
League Student <league@iMac-23.attlocal.net>
2019-12-08 12:54:06 -0800

    i coded

---
+++ src/_11_my_ages/MyAges.java
@@ -0,0 +1,9 @@
+package _11_my_ages;
+
+public class MyAges {
+public static void main(String[] args) {
+	for(int i=0;i<11;i++) {
+		System.out.println(i);
+	}
+}
+}
---
+++ src/_12_badgers/Badger.java
@@ -0,0 +1,14 @@
+package _12_badgers;
+
+public class Badger {
+public static void main(String[] args) {
+	for(int i=0;i<12;i++) {
+		System.out.println("Badger");
+		
+	}
+	for(int i=0;i<2;i++) {
+		System.out.println("mushroom");
+	}
+	
+}
+}
